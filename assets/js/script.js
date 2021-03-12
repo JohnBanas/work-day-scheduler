fa8f2c76ecd31e72f9e0d3168955a24d71361a79
JPB <jbanas9124@gmail.com>
2021-03-12 17:29:13 -0600

    working current day and time(relative to central time)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,45 @@
 //document ready
 $(function () {
   //moment time
-  const currentDay = moment().format('MMMM Do YYYY');
+  let currentDay = ""; 
 
   //navbar subtitle
-  let $subtitle = $('#currentDay');
-  $subtitle.text(currentDay);
+  changeTime = () => {
+    currentDay = moment().format('MMMM Do YYYY, hh:mm:ss a');
+    let $subtitle = $('#currentDay');
+    $subtitle.text(currentDay);
+  }
+  setInterval(changeTime, 1000);
+  
+  //create work-day hours am/pm
 
+  //create non-work-day hours am/pm
 
+  //check localStorage for timeBlock data and if there create <div> if not make empty []
+
+  //iterate through hours, if work day hours create <div>
+
+  //<div> needs to be able to change color based on if the hour is past, present, or future
+
+  //create container for elements of <div>
+
+  //put workday hours in left side (on md lg size, on sm left underneath)
+
+  //input text box in center (in between on lg md, on top of buttons on sm)
+
+  //save button on right side (on sm underneath right)
+
+  //<div> is pushed to timeBlock array with its data
+
+  //when input text in <div> is clicked the text data can be changed, if they click off the input, reverts back 
+
+  //after any changes, save to local storage
 });
 
-
-/* add <ul> in HTML to hold <li> input for time blocks
-for standard business day (in javascript)
-three parts to each <li> :
--1 time on the left (make that the data index?)
--2 text input in the center
--3 save button on the right end */
-
-//create array for <li>
-
-/* The time is checked on each time block and color
-coded to label it past, present, and future */
-
-/* you can edit time-block event text */
-
-/* when save button is clicked the event is saved in local storage */
+/* extras if possible?
+1- can the user adjust the time of the work day?
+2- can the user choose 1/2 hour or 1/4 hour time increments?
+3- can they adjust the colors of past present future time blocks?
+4- if a event takes up more than one time block can they be put togther into one large text area,
+and the time be changed? example = 7:30 - 8:30? */
 
