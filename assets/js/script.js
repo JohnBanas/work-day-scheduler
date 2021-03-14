7e4d61c454a3368b7d72ecd91f717f9a73a3e905
JPB <jbanas9124@gmail.com>
2021-03-14 14:54:24 -0500

    finished saving timeblocks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,368 +14,387 @@ $(function () {
   //create all hours am/pm in full array
   let fullDay = [
     {
-      id: "0",
+      id: '0',
+      militaryId: "00",
       hour: "12",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "1",
+      id: '1',
+      militaryId: "01",
       hour: "01",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "2",
+      id: '2',
+      militaryId: "02",
       hour: "02",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "3",
+      id: '3',
+      militaryId: "03",
       hour: "03",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "4",
+      id: '4',
+      militaryId: "04",
       hour: "04",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "4",
-      hour: "04",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "5",
+      id: '5',
+      militaryId: "05",
       hour: "05",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "6",
+      id: '6',
+      militaryId: "06",
       hour: "06",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "7",
+      id: '7',
+      militaryId: "07",
       hour: "07",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "8",
+      id: '8',
+      militaryId: "08",
       hour: "08",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "9",
+      id: '9',
+      militaryId: "09",
       hour: "09",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "10",
+      id: '10',
+      militaryId: "10",
       hour: "10",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "11",
+      id: '11',
+      militaryId: "11",
       hour: "11",
       minutes: ["00", "15", "30", "45"],
       ampm: "am",
       text: ""
     },
     {
-      id: "12",
+      id: '12',
+      militaryId: "12",
       hour: "12",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "13",
+      id: '13',
+      militaryId: "13",
       hour: "01",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "14",
+      id: '14',
+      militaryId: "14",
       hour: "02",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "15",
+      id: '15',
+      militaryId: "15",
       hour: "03",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "16",
+      id: '16',
+      militaryId: "16",
       hour: "04",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "17",
+      id: '17',
+      militaryId: "17",
       hour: "05",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "18",
+      id: '18',
+      militaryId: "18",
       hour: "06",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "19",
+      id: '19',
+      militaryId: "19",
       hour: "07",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "20",
+      id: '20',
+      militaryId: "20",
       hour: "08",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "21",
+      id: '21',
+      militaryId: "21",
       hour: "09",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "22",
+      id: '22',
+      militaryId: "22",
       hour: "10",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     },
     {
-      id: "23",
-      hour: "11",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    }
-  ];
-  let hoursAM = [
-    {
-      id: "0",
-      hour: "12",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "1",
-      hour: "01",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "2",
-      hour: "02",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "3",
-      hour: "03",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "4",
-      hour: "04",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "4",
-      hour: "04",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "5",
-      hour: "05",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "6",
-      hour: "06",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "7",
-      hour: "07",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "8",
-      hour: "08",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "9",
-      hour: "09",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "10",
-      hour: "10",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    },
-    {
-      id: "11",
-      hour: "11",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "am",
-      text: ""
-    }
-  ];
-  let hoursPM = [
-    {
-      id: "12",
-      hour: "12",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "13",
-      hour: "01",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "14",
-      hour: "02",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "15",
-      hour: "03",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "16",
-      hour: "04",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "17",
-      hour: "05",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "18",
-      hour: "06",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "19",
-      hour: "07",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "20",
-      hour: "08",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "21",
-      hour: "09",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "22",
-      hour: "10",
-      minutes: ["00", "15", "30", "45"],
-      ampm: "pm",
-      text: ""
-    },
-    {
-      id: "23",
+      id: '23',
+      militaryId: "23",
       hour: "11",
       minutes: ["00", "15", "30", "45"],
       ampm: "pm",
       text: ""
     }
   ];
+  /* two arrays for extra project later 
+   let hoursAM = [
+     {
+       id: "0",
+       hour: "12",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "1",
+       hour: "01",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "2",
+       hour: "02",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "3",
+       hour: "03",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "4",
+       hour: "04",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "4",
+       hour: "04",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "5",
+       hour: "05",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "6",
+       hour: "06",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "7",
+       hour: "07",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "8",
+       hour: "08",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "9",
+       hour: "09",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "10",
+       hour: "10",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     },
+     {
+       id: "11",
+       hour: "11",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "am",
+       text: ""
+     }
+   ];
+   let hoursPM = [
+     {
+       id: "12",
+       hour: "12",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "13",
+       hour: "01",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "14",
+       hour: "02",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "15",
+       hour: "03",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "16",
+       hour: "04",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "17",
+       hour: "05",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "18",
+       hour: "06",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "19",
+       hour: "07",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "20",
+       hour: "08",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "21",
+       hour: "09",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "22",
+       hour: "10",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     },
+     {
+       id: "23",
+       hour: "11",
+       minutes: ["00", "15", "30", "45"],
+       ampm: "pm",
+       text: ""
+     }
+   ];*/
 
   //10 hour work day container array
   let workDayHours = [];
 
-  //allow user to change the integers in this workday function
+  //allow user to change the integers in this workday function(later for extras)
   workDay = () => {
     fullDay.forEach(function (e) {
       if (e.id >= 9 && e.ampm === 'am' || e.id <= 18 && e.ampm === 'pm') {
+
         workDayHours.push(e);
       }
     })
@@ -387,97 +406,88 @@ $(function () {
     localStorage.setItem("workDayHours", JSON.stringify(workDayHours));
   }
 
-  //check localStorage for timeBlock data and if there is create, if not make empty [];
+  //pulls from local storage to display event
+  storageEvents = () => {
+    workDayHours.forEach(function (_currentHour) {
+      $(`#${_currentHour.id}`).val(_currentHour.text);
+    })
+  }
+
+  //check localStorage for timeBlock data and if there is create,
   pullEvents = () => {
-    JSON.parse(localStorage.getItem("workDayHours"));
+    let pulledHours = JSON.parse(localStorage.getItem("workDayHours"));
+    if (pulledHours) {
+      workDayHours = pulledHours;
+    }
+    saveEvents();
+    storageEvents();
   }
 
   //iterate through work day hours create <form> for row & input
-
   workDayHours.forEach(function (currentHour) {
-    let eventRow = $('<form>').attr({ 'class': 'eventRow row', 'id':`${currentHour.id}` });
+    let eventRow = $('<form>').attr({ 'class': 'eventRow row' });
+    $('.container').append(eventRow);
     //put workday hours in left side (on md lg size, on sm left underneath)
     let eventTime = $('<div>').text(`${currentHour.hour}:${currentHour.minutes[0]}${currentHour.ampm}`).attr({ 'class': ' col-2 hour eventTime' });
     //event text input container
     let eventDataContainer = $('<span>').attr({ 'class': 'col-8 time-block eventDataContainer' });
     //event text
-    let eventText = $('<textarea>').attr({ 'class': 'eventText' });
-    eventText.text = currentHour.text;
+    let eventText = $('<textarea>').attr({ 'class': 'text', 'id': currentHour.id });
+    eventDataContainer.append(eventText);
+    //event text
+    eventText.attr({ 'class': 'eventText' });
 
     //<div> needs to be able to change color based on if the hour is past, present, or future
-    // compare real time to time on schedule
-    eventDataContainer.append(eventText);
-    if (moment().format('a') === 'am' && currentHour.ampm !== 'am' && currentHour.id === '0') {
-      eventText.attr({ 'class': 'future' });
-    } else
-      if (moment().format('a') === 'am' && currentHour.ampm === 'am') {
-        if (currentHour.hour < moment().format('hh') && moment().format('hh') !== 12 && currentHour.hour !== 12) {
-          eventText.attr({ 'class': 'past' })
-        } else
-          if (moment().format('hh') === currentHour.hour) {
-            eventText.attr({ 'class': 'present' });
-          } else
-            if (moment().format('hh') === 12 && currentHour.hour < moment().format('hh')) {
-              eventText.attr({ 'class': 'future' });
-            } else
-              if (currentHour.hour > moment().format('hh') && moment().format('hh') !== 12 && currentHour.hour !== 12) {
-                eventText.attr({ 'class': 'future' });
-              }
-      }
-
-    if (moment().format('a') === 'pm' && currentHour.ampm !== 'pm'|| currentHour.id === '12') {
-      eventText.attr({ 'class': 'past' });
-    } else
-      if (moment().format('a') === 'pm' && currentHour.ampm === 'pm') {
-        if (currentHour.hour < moment().format('hh')) {
-          eventText.attr({ 'class': 'past' })
-        } else {
-          eventText.attr({ 'class': 'future' })
-        }
-        if (moment().format('hh') === currentHour.hour) {
-          eventText.attr({ 'class': 'present' });
-        }
-        if (moment().format('hh') === 12 && currentHour.hour < moment().format('hh')) {
-          eventText.attr({ 'class': 'future' });
-        } 
-        if (currentHour.hour > moment().format('hh') && currentHour.id !== '0') {
-          eventText.attr({ 'class': 'future' });
-        }
-      }
+    // compare real time to time on schedule (military times for 
+    //logistical problems with AM / PM having the same numbers)
+    if (currentHour.id < moment().format('HH')) {
+      eventText.attr({ 'class': 'past text' });
+    } else if (currentHour.id === moment().format('HH')) {
+      eventText.attr({ 'class': 'present text' });
+    } else if (currentHour.id > moment().format('HH')) {
+      eventText.attr({ 'class': 'future text' });
+    }
     //create save button
-    let saveEventBtnContainer = $('<span>').attr({ 'class': 'col-1 btnID' });
-    let saveEventBtn = $('<button>').attr({ 'class': 'saveBtn', 'type':'submit' });
+    let saveEventBtnContainer = $('<span>').attr({ 'class': 'col-1' });
+    let saveEventBtn = $('<button>').attr({ 'class': 'saveBtn', 'type': 'submit', 'id': `${currentHour.id}` });
     let saveBtnImage = $('<img>').attr({ 'src': '../assets/images/cogs.svg', 'alt': 'Tow gears', 'class': 'iconInject' })
     let saveBtnText = $('<span>').text('Save').attr({ 'class': 'btnText' });
     saveEventBtnContainer.append(saveEventBtn.append(saveBtnImage, saveBtnText));
     //form container for time block
     eventRow.append(eventTime, eventDataContainer, saveEventBtnContainer);
-    $('#timeBlockContainer').append(eventRow);
+    //$('#timeBlockContainer').append(eventRow);
   })
 
-  //edit the text area
-  $('.time-block').on('click', 'textarea', function (workDayHours) {
-    let text = $(this).val();
-    let id = $(this).closest('.eventRow').index();
-    //this works so far, now we need to get the value of the text
-    //into the array and pull out that value on page reload
+  pullEvents();
+
+  $('.text').on('blur', function () {
+    let text = $(this).val().trim();
+    let textIndex = $(this).attr('id');
+    workDayHours.forEach(function (i) {
+      if (i.id === textIndex) {
+        i.text = text;
+      }
+    })
   })
 
-  $('#timeBlockContainer').on('click', 'button', function () {
-    
-  });
+
+  $(".saveBtn").on('click', function (event) {
+    event.preventDefault();
+    saveEvents();
+    storageEvents();
+  })
 
   let imageInject = $('.iconInject');
 
   SVGInjector(imageInject);
 
   let windowSize = window.matchMedia('(max-width: 980px)')
-  
-  
 
-  
 
-  
+
+
+
+
 
 
 
